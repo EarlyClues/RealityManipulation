54d31dd24d5aafb6c58149c03ba6c93ff7747225
RICHARD STALLMANS RIDER <deprecatedapi@inbox.com>
2013-04-11 18:34:17 -0700

    adding a legacy prayer for reference

---
+++ prayers/legacy-blackberry-development-prayer.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+#  This prayer script was used in 2008 while trying (and mostly failing)
+#  to do Black Berry software development.  Pairing the this script with literal
+#  burning of sage at my desk also made little difference. Your mileage may vary.
+
+
+# Usage
+# $ ruby legacy-blackberry-development-prayer.rb
+
+while (true)
+    puts "I pray that the phone will finally work."
+    sleep(1)
+end
